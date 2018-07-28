966666c531777c634f0b86cb30679b468d9fdaa6
mattritani <33272889+mattritani@users.noreply.github.com>
2018-07-29 10:52:36 +1200

    Update eloquent.js

--- eloquent.js
+++ eloquent.js
@@ -39,3 +39,41 @@ console.log(isEven(-1));
 // → ??
 
 // i realise that this does also make all numbers negative not even - however in the context and framwork of the problem at hand it does solve it. 
+
+
+//--------------------------------------
+
+Bean counting
+
+// Counting Bs
+
+function countBs(str) {
+    var numberOfBs = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === "B") {
+            numberOfBs++;
+
+        }
+    }
+        return (numberOfBs); 
+}
+
+// Counting Number of selected charater
+
+function countChar(str, nominateCharacter) {
+    var numberCharacter = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === nominateCharacter) {
+            numberCharacter++;
+        }
+    }
+            return (numberCharacter); 
+}
+
+
+
+console.log(countBs("BBCBBDDBB"));
+// → 2
+console.log(countChar("kakaakearalak", "a"));
+// → 4
+
